6f3955b849387ba216210c08d2bde3d9c2497781
James Richmond <jblackenmusic@gmail.com>
2024-03-01 23:41:40 -0700

    wolfram automata completed

--- p5doc.js
+++ p5doc.js
@@ -1,50 +1,46 @@
 cells = []
-let w = 10
+let ruleValue = 110;
+let ruleSet;
+let w = 5;
 let y = 0;
 
 function setup() {
-  createCanvas(430, 390);
+  createCanvas(600, 1800)
+  frameRate(30);
+  
+  ruleSet = ruleValue.toString(2).padStart(8, "0");
+  
   let total = width / w;
   for (let i = 0; i < total; i++) {
-    cells[i] = 1
-    
+    cells[i] = 0;
   }
-  cells[floor(total/2)] = 0
-  background(255);
-    noLoop();
-  
-  console.log('asdf');
-  console.log(cells);
+  cells[floor(total/2)] = 1;
 }
 
-function draw() {
-    
-  let nextCells = [];
-  nextCells[0] = cells[0];
-  nextCells[0] = calculateState(1, 1, 1);
-  nextCells[cells.length - 1] = calculateState(1, 1, 1);
-  
+function draw() {    
   for (let i = 0; i < cells.length; i++) {
     let x = i * w;
-    stroke(0);
+    noStroke();
     fill(255 - cells[i] * 255);
     square(x, y, w);
-    
-    let left = cells[i - 1];
-    let right = cells[i + 1];
-    
+  }
+  
+  y += w;
+  
+  let nextCells = [];
+  // nextCells[0] = cells[0];
+  // nextCells[cells.length - 1] = cells[cells.length - 1];
+  
+  let len = cells.length
+  for (let i = 0; i < cells.length; i++) {
+    let left = cells[(i - 1 + len) % len];
+    let right = cells[(i + 1 + len) % len];
     let state = cells[i];
     let newState = calculateState(left, state, right);
-    
-    if (nextCells[i] == undefined) {
-      nextCells[i] = 0;}
-      console.log(0);
-    }    
+    nextCells[i] = newState;
+  }
   
-
   cells = nextCells;
-  y += w;
-  noLoop()
 }
 
 function mousePressed() {
@@ -52,12 +48,17 @@ function mousePressed() {
 }
 
 function calculateState(a, b, c) {
-  if (a == 1 && b == 1 && c == 1) return 1
-  if (a == 1 && b == 1 && c == 0) return 1
-  if (a == 1 && b == 0 && c == 1) return 1
-  if (a == 1 && b == 0 && c == 0) return 1
-  if (a == 0 && b == 1 && c == 1) return 1
-  if (a == 0 && b == 1 && c == 0) return 1
-  if (a == 0 && b == 0 && c == 1) return 1
-  if (a == 0 && b == 0 && c == 0) return 1
+  let neighborhood = '' + a + b + c;
+  let value = 7 - parseInt(neighborhood, 2);
+  
+  return parseInt(ruleSet[value]);
+  
+//   if (a == 1 && b == 1 && c == 1) return 1
+//   if (a == 1 && b == 1 && c == 0) return 0
+//   if (a == 1 && b == 0 && c == 1) return 1
+//   if (a == 1 && b == 0 && c == 0) return 1
+//   if (a == 0 && b == 1 && c == 1) return 0
+//   if (a == 0 && b == 1 && c == 0) return 1
+//   if (a == 0 && b == 0 && c == 1) return 1
+//   if (a == 0 && b == 0 && c == 0) return 0
 }
